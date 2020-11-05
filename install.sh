d4d7462ff6b4ab49cc7eb4e9a72e7f23a3025df3
pedromtelho <pedromtelho@hotmail.com>
2020-11-05 17:50:46 -0300

    testing new superuser

--- install.sh
+++ install.sh
@@ -3,6 +3,6 @@ python3 -m pip install -r tasks/requirements.txt
 python3 tasks/manage.py migrate
 echo '@reboot cd /home/ubuntu/tasks && ./run.sh' | crontab
 export DJANGO_SUPERUSER_PASSWORD=cloud
-export DJANGO_SUPERUSER_USERNAME=cloud
+export DJANGO_SUPERUSER_USERNAME=cloudx
 export DJANGO_SUPERUSER_EMAIL=cloud@a.com
 python3 tasks/manage.py createsuperuser --noinput
