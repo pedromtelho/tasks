7a4618d33acda00865cf5f69e817c39e76b5d7d0
pedromtelho <pedromtelho@hotmail.com>
2020-11-06 10:06:54 -0300

    sudo reboot into install sh

--- install.sh
+++ install.sh
@@ -6,3 +6,4 @@ export DJANGO_SUPERUSER_PASSWORD=cloud
 export DJANGO_SUPERUSER_USERNAME=cloudx
 export DJANGO_SUPERUSER_EMAIL=cloud@a.com
 python3 tasks/manage.py createsuperuser --noinput
+sudo reboot
